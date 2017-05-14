a1026bbe602876b8eff52d62dd1139a29300fb37
sajan <sjnmishra8@gmail.com>
2017-05-14 13:08:16 +0530

    added initial files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import GitHub_Logo from '../assets/GitHub_Logo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={GitHub_Logo} className="App-logo" alt="logo" />
+          <h2>Welcome to GitHub-Search</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
