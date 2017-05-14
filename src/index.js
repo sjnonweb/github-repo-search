64d2abab6d4798e1dd1ede33b44d6a3928caa64b
sajan <sjnmishra8@gmail.com>
2017-05-14 13:22:34 +0530

    restructing code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import App from './App';
 import './index.css';
 
+const root = document.getElementById('root');
+
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  root
 );
